24b00ac3489eb500b0cb698bc93152a16edb86f6
Adenny Fernandes <70215330+adennyfernandesphp@users.noreply.github.com>
2020-11-18 20:58:59 -0300

    up

---
+++ Public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+function getConnection(){
+
+    $dsn = 'mysql:host=localhost;dbname=cursoifes';
+    $user = 'root';
+    $pass = 'root';
+
+    try {
+
+        $pdo = new PDO($dsn, $user, $pass );
+
+        return $pdo;
+    } catch (PDOException $ex) {
+
+    echo 'erro: '.$ex->getmessage();
+
+    }
+
+}
